1b2872afc19b632158978385626e7e6d32bc59d4
Ondor-R <rehanondor@gmail.com>
2025-12-15 01:06:04 +0700

    fix import css name

--- op-timis_inventory/frontend/src/Login.jsx
+++ op-timis_inventory/frontend/src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import './style.css';
 
 function Login() {
   const [email, setEmail] = useState('');
