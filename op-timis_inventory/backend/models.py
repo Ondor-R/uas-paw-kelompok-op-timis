665b2a276f15b087719fa5928219eceb629bb36c
Ondor-R <rehanondor@gmail.com>
2025-12-14 20:12:33 +0700

    tweak a lil models.py

--- op-timis_inventory/backend/models.py
+++ op-timis_inventory/backend/models.py
@@ -1,12 +1,11 @@
-from sqlalchemy import (
-    Column, Integer, String, Float, Date, ForeignKey, create_engine, Text
-)
+from sqlalchemy import (Column, Integer, String, Float, Date, ForeignKey, create_engine, Text)
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import Boolean
 
-# Base class untuk semua model
+#Base class untuk semua model
 Base = declarative_base()
 
-# 1. Tabel Users
+#Tabel Users
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -14,8 +13,9 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     role = Column(String, nullable=False) # 'admin' atau 'staff'
+    is_active = Column(Boolean, default=False)
 
-# 2. Tabel Products
+#Tabel Products
 class Product(Base):
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True)
@@ -26,31 +26,34 @@ class Product(Base):
     stock = Column(Integer, default=0)
     min_stock = Column(Integer, default=0)
     
-    # Relasi agar bisa ngambil data transaksi terkait produk ini
+    # Relasi
     transactions = relationship("Transaction", back_populates="product")
 
-# 3. Tabel Suppliers
+#Tabel Suppliers
 class Supplier(Base):
     __tablename__ = 'suppliers'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    contact = Column(String) #ini harusnya int tapi sudah terlanjur string, it is what it is
+    contact = Column(String)
     email = Column(String)
 
-# 4. Tabel Transactions (Stock In/Out)
+#Tabel Transactions (Stock In/Out)
 class Transaction(Base):
     __tablename__ = 'transactions'
     id = Column(Integer, primary_key=True)
     product_id = Column(Integer, ForeignKey('products.id'))
-    type = Column(String, nullable=False) # 'in' (purchase) atau 'out' (sales)
+    supplier_id = Column(Integer, ForeignKey('suppliers.id'), nullable=True) 
+
+    type = Column(String, nullable=False)
     quantity = Column(Integer, nullable=False)
     date = Column(Date, nullable=False)
     notes = Column(Text)
 
-    # Relasi ke Product
+    # Relasi
     product = relationship("Product", back_populates="transactions")
+    supplier = relationship("Supplier")
 
-# 5. Tabel Product_Suppliers
+#Tabel Product_Suppliers
 class ProductSupplier(Base):
     __tablename__ = 'product_suppliers'
     id = Column(Integer, primary_key=True)
