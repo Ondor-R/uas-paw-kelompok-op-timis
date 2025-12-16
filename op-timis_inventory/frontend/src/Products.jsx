d935e9e4074ec1372d20cc85933610f3dbd395d1
Awesome1209 <awi.123140201@student.itera.ac.id>
2025-12-16 20:46:13 +0700

    buat fungsi fetchProducts

--- op-timis_inventory/frontend/src/Products.jsx
+++ op-timis_inventory/frontend/src/Products.jsx
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './style.css';
 
 function Products() {
   const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const res = await axios.get('http://localhost:6543/products');
+      setProducts(res.data);
+    } catch (error) {
+      console.error("Gagal ambil data", error);
+    }
+  };
 
   return (
     <div className="page-container">
