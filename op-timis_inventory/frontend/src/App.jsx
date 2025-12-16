78a979b9c835de19311fcc4081edb6cd9a6248da
211-Bimas <muhammad.123140211@student.itera.ac.id>
2025-12-16 21:02:44 +0700

    buat route path products.jsx

--- op-timis_inventory/frontend/src/App.jsx
+++ op-timis_inventory/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import Products from './Products';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         {/* route-route nya*/}
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
   );
